1f8c313481db45d724f18ea41715d4a58e4ed5a6
Pardha Saradhi Sastry Nemani <122751514+pnemani0193@users.noreply.github.com>
2023-03-05 18:40:08 -0600

    first commit for gcloud command

---
+++ pn_assignment10/vm_create.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env/ bash
+echo $1 > project-file
+echo $2 > token-file
+echo $3 > config-file
+gcloud compute instances create pn-in-1 --project=project-file --access-token-file=token-file config-file
