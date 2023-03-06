50074f841d0f588b45930b82f13befa5d145c88e
Pardha Saradhi Sastry Nemani <122751514+pnemani0193@users.noreply.github.com>
2023-03-05 19:59:44 -0500

    second commit for the dev

--- pn_assignment10/vm_create.sh
+++ pn_assignment10/vm_create.sh
@@ -2,4 +2,4 @@
 echo $1 > project-file
 echo $2 > token-file
 echo $3 > config-file
-gcloud compute instances create pn-in-1 --project=project-file --access-token-file=token-file config-file
+wsl gcloud compute instances create pn-in-1 --project=project-file --access-token-file=token-file config-file
